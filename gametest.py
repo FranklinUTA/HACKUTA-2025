61770eed2548e59d12a9c334f77ea17680bd1a51
nixonthong <nxt8383@mavs.uta.edu>
2025-10-04 16:50:31 -0500

    testgame

---
+++ gametest.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Simple Game")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.update()
+
+pygame.quit()
+
