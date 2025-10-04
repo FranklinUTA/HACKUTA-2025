c764bbc462ee224c7381c7c4e6a979ce85385ced
guymine1 <anguyenminh23@gmail.com>
2025-10-04 15:16:21 -0500

    backend progress

--- app.py
+++ app.py
@@ -1,9 +1,52 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect
+
+import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+def initialdb():
+    with sqlite3.connect('workout.db') as conn:
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS entries
+                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      date TEXT,
+                      exercise TEXT,
+                      timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+                      set INTEGER,
+                      reps INTEGER,
+                      weight REAL,
+                      notes TEXT)''')  # Added missing comma
+        conn.commit()
+        conn.close()
 
-def home():
-    return "first Flask app"
+@app.route('/')
+def index():
+    with sqlite3.connect('workout.db') as conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM entries ORDER BY date DESC, timestamp DESC")
+
+        entries = c.fetchall()
+        return render_template('index.html', entries=entries)
+    conn.close()
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    with sqlite3.connect('workout.db') as conn:
+        c = conn.cursor()
+        date = request.form['date']
+        exercise = request.form['exercise']
+        set = request.form['set']
+        repts = request.form['reps']
+        weight = request.form['weight']
+        notes = request.form['notes']
+        c.execute("INSERT INTO entries (date, exercise, set, reps, weight, notes)")
+        
+        
+
+    
+    
+    
+
+
 
