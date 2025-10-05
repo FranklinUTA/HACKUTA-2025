f8b91263a1d54fd37eb4040ec06fb946eeca258f
Nixon <nxt8383@mavs.uta.edu>
2025-10-05 00:29:06 -0500

    centered quit button

--- freakybird_tongue.py
+++ freakybird_tongue.py
@@ -209,7 +209,7 @@ pipe_group = pygame.sprite.Group()
 freaky = Bird(100, int(screen_height / 4))
 bird_group.add(freaky)
 button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
-quit = Button(screen_width // 2 - 80, screen_height // 2 - 30, quit_img)
+quit = Button(screen_width // 2 - 33, screen_height // 2 - 30, quit_img)
 
 # --- Game loop ---
 run = True
